02eb8e0db6626905149cada91f2662a4e75610fe
Waqas Munir <devwaqas232@gmail.com>
2024-10-05 15:50:23 +0500

    Fixed Bug

--- controller/product.ts
+++ controller/product.ts
@@ -5,10 +5,7 @@ import Product from "../models/product";
 const createProduct = async (req: Request, res: Response) => {
   const { title, category, description, price } = req.body;
   try {
-    if (!req.file)
-      return res.status(404).json({ message: "Image is required", ok: false });
-
-    const result = await cloudinary.uploader.upload(req.file.path);
+    const result = await cloudinary.uploader.upload(req.file!.path);
     const product = await Product.create({
       title,
       category,
